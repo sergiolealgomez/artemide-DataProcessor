4511e800e3fa6c0717d343362b48a599ccd14060
Alexey Vladimirov <vladimirov.aleksey@gmail.com>
2021-04-09 09:34:38 +0200

    The code for TMD-PDF is updated for new minuit, and CJ15+MSHT20

--- FittingPrograms/PDF-TMD/LMFit_PDFrep_EXPcentral.py
+++ FittingPrograms/PDF-TMD/LMFit_PDFrep_EXPcentral.py
@@ -12,10 +12,8 @@
 #######################################################################
 
 #PDFinUse="HERA20"
-#PDFinUse="NNPDF31"
+PDFinUse="NNPDF31"
 #PDFinUse="CT18"
-#PDFinUse="MSHT20"
-PDFinUse="CJ15"
 
 ## if this trigger is ON the LHC data will be fit only by shape
 useNormalizedLHCdata=False
@@ -25,23 +23,20 @@ useA7data=False
 splitUpsilon=True
 ## Use the reduced set of the data
 useReducedSet=True
-## Use penalty term for too different flavors
-usePenaltyTerm=True
 
 ## total number of parameters in TMDPDF (7 and 12 case are defined)
 numberOfParameters=12
 
 #### Starting and final replica (included)
-StartReplica=3
+StartReplica=1
 FinalReplica=3
 
 ## automatic name generation for the run
-runName="model5.0_"+PDFinUse+"_PDFrep_"
+runName="model4.0_"+PDFinUse+"_PDFrep_"
 if (not useA7data): runName+="noA7_"
 if (splitUpsilon): runName+="spltUPS_"
 if (useNormalizedLHCdata): runName+="norm_"
 if (useReducedSet): runName+="reducedSet_"
-if (usePenaltyTerm): runName+="penaltyTerm_"
 if (runName[-1]=="_"): runName=runName[0:-1]
 
 print(" RUN: "+runName)
@@ -182,69 +177,6 @@ dropPoints=["A7-10y20.7", "A7-10y20.8", "A7-10y20.9", "A7-20y24.4", "A7-20y24.5"
 "LHCb8.10", "LHCb8.8", "LHCb8.9", "PHE200.2",\
 ##### extra points by hands
 "A8-116Q150.0","CDF1.0","CDF1.1","CDF1.2","D01.0", "D01.1", "D02.0","CMS7.0","LHCb13.0"]
-    
-includePoints=[
-    'CDF2.0', 'CDF2.1', 'CDF2.2', 'CDF2.3', 'CDF2.4', 'CDF2.5', 'CDF2.6', 'CDF2.7',
-    'CDF2.8', 'CDF2.9', 'CDF2.10', 'CDF2.11', 'CDF2.12', 'CDF2.13', 'CDF2.14',
-    'D02m.0', 'D02m.1', 'D02m.2',
-    'A8-00y04.0', 'A8-00y04.1', 'A8-00y04.2', 'A8-00y04.3', 'A8-00y04.4', 'A8-04y08.0',
-    'A8-04y08.1', 'A8-04y08.2', 'A8-04y08.3', 'A8-04y08.4', 'A8-08y12.0', 'A8-08y12.1',
-    'A8-08y12.2', 'A8-08y12.3', 'A8-08y12.4', 'A8-12y16.0', 'A8-12y16.1', 'A8-12y16.2',
-    'A8-12y16.3', 'A8-12y16.4', 'A8-16y20.0', 'A8-16y20.1', 'A8-16y20.2', 'A8-16y20.3',
-    'A8-16y20.4', 'A8-20y24.0', 'A8-20y24.1', 'A8-20y24.2', 'A8-20y24.3', 'A8-20y24.4',
-    'A8-46Q66.0', 'A8-46Q66.1', 'A8-46Q66.2',
-    'LHCb7.0', 'LHCb7.1', 'LHCb7.5', 'LHCb7.6', 'LHCb8.0', 'LHCb8.1',
-    'LHCb8.2', 'LHCb8.3', 'LHCb8.4', 'LHCb8.5', 'LHCb8.6', 'PHE200.0', 'PHE200.1',
-    'E228-200.4Q5.0', 'E228-200.4Q5.1', 'E228-200.4Q5.2', 'E228-200.4Q5.3', 'E228-200.4Q5.4',
-    'E228-200.4Q5.5', 'E228-200.5Q6.0', 'E228-200.5Q6.1', 'E228-200.5Q6.2', 'E228-200.5Q6.3',
-    'E228-200.5Q6.4', 'E228-200.5Q6.5', 'E228-200.5Q6.6', 'E228-200.6Q7.0', 'E228-200.6Q7.1',
-    'E228-200.6Q7.2', 'E228-200.6Q7.3', 'E228-200.6Q7.4', 'E228-200.6Q7.5', 'E228-200.6Q7.6',
-    'E228-200.6Q7.7', 'E228-200.6Q7.8', 'E228-200.7Q8.0', 'E228-200.7Q8.1', 'E228-200.7Q8.2',
-    'E228-200.7Q8.3', 'E228-200.7Q8.4', 'E228-200.7Q8.5', 'E228-200.7Q8.6', 'E228-200.7Q8.7',
-    'E228-200.7Q8.8', 'E228-200.7Q8.9', 'E228-200.8Q9.0', 'E228-200.8Q9.1', 'E228-200.8Q9.2',
-    'E228-200.8Q9.3', 'E228-200.8Q9.4', 'E228-200.8Q9.7', 'E228-200.8Q9.8', 'E228-300.4Q5.0<u',
-    'E228-300.4Q5.1<u', 'E228-300.4Q5.2<u', 'E228-300.4Q5.3<u', 'E228-300.4Q5.4<u', 'E228-300.4Q5.5<u',
-    'E228-300.5Q6.0<u', 'E228-300.5Q6.1<u', 'E228-300.5Q6.2<u', 'E228-300.5Q6.3<u', 'E228-300.5Q6.4<u',
-    'E228-300.5Q6.5<u', 'E228-300.5Q6.6<u', 'E228-300.6Q7.0<u', 'E228-300.6Q7.1<u', 'E228-300.6Q7.2<u',
-    'E228-300.6Q7.3<u', 'E228-300.6Q7.4<u', 'E228-300.6Q7.5<u', 'E228-300.6Q7.6<u', 'E228-300.6Q7.7<u',
-    'E228-300.6Q7.8<u', 'E228-300.7Q8.0<u', 'E228-300.7Q8.1<u', 'E228-300.7Q8.2<u', 'E228-300.7Q8.3<u',
-    'E228-300.7Q8.4<u', 'E228-300.7Q8.5<u', 'E228-300.7Q8.6<u', 'E228-300.7Q8.7<u', 'E228-300.7Q8.8<u',
-    'E228-300.7Q8.9<u', 'E228-300.8Q9.0<u', 'E228-300.8Q9.1<u', 'E228-300.8Q9.2<u', 'E228-300.8Q9.3<u',
-    'E228-300.8Q9.4<u', 'E228-300.8Q9.5<u', 'E228-300.8Q9.6<u', 'E228-300.8Q9.7<u', 'E228-300.8Q9.8<u',
-    'E228-300.8Q9.9<u', 'E228-300.8Q9.10<u', 'E228-300.11Q12.0>u', 'E228-300.11Q12.1>u',
-    'E228-300.11Q12.2>u', 'E228-300.11Q12.3>u', 'E228-300.11Q12.4>u', 'E228-300.11Q12.5>u',
-    'E228-300.11Q12.6>u', 'E228-300.11Q12.7>u', 'E228-300.11Q12.8>u', 'E228-300.11Q12.11>u',
-    'E228-400.5Q6.0<u', 'E228-400.5Q6.1<u', 'E228-400.5Q6.2<u', 'E228-400.5Q6.3<u', 'E228-400.5Q6.4<u',
-    'E228-400.5Q6.5<u', 'E228-400.5Q6.6<u', 'E228-400.6Q7.0<u', 'E228-400.6Q7.1<u', 'E228-400.6Q7.2<u',
-    'E228-400.6Q7.3<u', 'E228-400.6Q7.4<u', 'E228-400.6Q7.5<u', 'E228-400.7Q8.0<u', 'E228-400.7Q8.1<u',
-    'E228-400.7Q8.2<u', 'E228-400.7Q8.3<u', 'E228-400.7Q8.4<u', 'E228-400.7Q8.5<u', 'E228-400.7Q8.6<u',
-    'E228-400.7Q8.7<u', 'E228-400.7Q8.8<u', 'E228-400.7Q8.9<u', 'E228-400.8Q9.0<u', 'E228-400.8Q9.1<u',
-    'E228-400.8Q9.2<u', 'E228-400.8Q9.3<u', 'E228-400.8Q9.4<u', 'E228-400.8Q9.5<u', 'E228-400.8Q9.6<u',
-    'E228-400.8Q9.7<u', 'E228-400.8Q9.8<u', 'E228-400.8Q9.9<u', 'E228-400.8Q9.10<u', 'E228-400.11Q12.0>u',
-    'E228-400.11Q12.1>u', 'E228-400.11Q12.2>u', 'E228-400.11Q12.3>u', 'E228-400.11Q12.4>u',
-    'E228-400.11Q12.5>u', 'E228-400.11Q12.6>u', 'E228-400.11Q12.7>u', 'E228-400.11Q12.8>u',
-    'E228-400.11Q12.9>u', 'E228-400.11Q12.10>u', 'E228-400.11Q12.11>u', 'E228-400.11Q12.12>u',
-    'E228-400.11Q12.13>u', 'E228-400.11Q12.14>u', 'E228-400.12Q13.0>u', 'E228-400.12Q13.1>u',
-    'E228-400.12Q13.2>u', 'E228-400.12Q13.3>u', 'E228-400.12Q13.4>u', 'E228-400.12Q13.5>u',
-    'E228-400.12Q13.6>u', 'E228-400.12Q13.7>u', 'E228-400.12Q13.8>u', 'E228-400.12Q13.9>u',
-    'E228-400.12Q13.10>u', 'E228-400.12Q13.11>u', 'E228-400.12Q13.13>u', 'E228-400.12Q13.14>u',
-    'E228-400.12Q13.15>u', 'E228-400.13Q14.0>u', 'E228-400.13Q14.1>u', 'E228-400.13Q14.2>u',
-    'E228-400.13Q14.3>u', 'E228-400.13Q14.4>u', 'E228-400.13Q14.5>u', 'E228-400.13Q14.6>u',
-    'E228-400.13Q14.7>u', 'E228-400.13Q14.8>u', 'E228-400.13Q14.9>u', 'E228-400.13Q14.11>u',
-    'E228-400.13Q14.12>u', 'E772.11Q12.0', 'E772.11Q12.1', 'E772.11Q12.2', 'E772.11Q12.3',
-    'E772.11Q12.4', 'E772.11Q12.5', 'E772.11Q12.6', 'E772.11Q12.7', 'E772.11Q12.8', 'E772.11Q12.9',
-    'E772.11Q12.10', 'E772.12Q13.0', 'E772.12Q13.1', 'E772.12Q13.2', 'E772.12Q13.3', 'E772.12Q13.4',
-    'E772.12Q13.5', 'E772.12Q13.7', 'E772.13Q14.0', 'E772.13Q14.1', 'E772.13Q14.2', 'E772.13Q14.3',
-    'E772.13Q14.6', 'E772.14Q15.1', 'E605.7Q8.0<u', 'E605.7Q8.1<u', 'E605.7Q8.2<u', 'E605.7Q8.3<u',
-    'E605.7Q8.4<u', 'E605.7Q8.5<u', 'E605.7Q8.6<u', 'E605.7Q8.7<u', 'E605.7Q8.8<u', 'E605.7Q8.9<u',
-    'E605.8Q9.0<u', 'E605.8Q9.1<u', 'E605.8Q9.2<u', 'E605.8Q9.3<u', 'E605.8Q9.4<u', 'E605.8Q9.5<u',
-    'E605.8Q9.6<u', 'E605.8Q9.7<u', 'E605.8Q9.8<u', 'E605.8Q9.9<u', 'E605.8Q9.10<u', 'E605.11Q13.0>u',
-    'E605.11Q13.1>u', 'E605.11Q13.2>u', 'E605.11Q13.3>u', 'E605.11Q13.4>u', 'E605.11Q13.5>u',
-    'E605.11Q13.6>u', 'E605.11Q13.7>u', 'E605.11Q13.8>u', 'E605.11Q13.9>u', 'E605.11Q13.10>u',
-    'E605.11Q13.11>u', 'E605.11Q13.12>u', 'E605.11Q13.13>u', 'E605.11Q13.14>u', 'E605.11Q13.15>u',
-    'E605.11Q13.16>u', 'E605.13Q18.0>u', 'E605.13Q18.1>u', 'E605.13Q18.2>u', 'E605.13Q18.3>u',
-    'E605.13Q18.4>u', 'E605.13Q18.5>u', 'E605.13Q18.6>u', 'E605.13Q18.7>u', 'E605.13Q18.8>u',
-    'E605.13Q18.9>u', 'E605.13Q18.10>u', 'E605.13Q18.11>u', 'E605.13Q18.12>u', 'E605.13Q18.13>u', 'E605.13Q18.14>u']
 
 #### Check the point kinematics
 def cutFunc0(p):    
@@ -252,8 +184,7 @@ def cutFunc0(p):
     par=1.0
 
     if(p["xSec"]>0):
-        #err=numpy.sqrt(sum([i**2 for i in p["uncorrErr"]]))/p["xSec"]
-        err=10000.0 ## I would like to add all points into the full set
+        err=numpy.sqrt(sum([i**2 for i in p["uncorrErr"]]))/p["xSec"]
     else:
         err=100.
     delta=p["<qT>"]/p["<Q>"]
@@ -289,7 +220,7 @@ def cutFunc0(p):
 def cutFunc(p):    
     
     #### check against the presence in the reduced set.
-    if(useReducedSet and not(p["id"] in includePoints)):
+    if(useReducedSet and p["id"] in dropPoints):
         return False,p
     
     return cutFunc0(p)
@@ -374,103 +305,78 @@ if(PDFinUse=="CT18"):
     #initialValues=(2.000, 0.042, 0.094, 12.534, 0.293, 0.004, 0.003, 16.568, 819.267) #model 2.2 CT18
     #initialValues=(2.000, 0.042, 0.121, 9.626,  0.289, 0.000, 0.000, 17.820, 524.722) #model 2.2 CT18 reduced
     #initialValues=(2.000, 0.042, 0.121, 9.626,  0.289, 0.000, 0.121, 9.626,  0.289, 0.000, 0.000, 17.820, 524.722, 0.) #model 4.0 CT18
-    initialValues=(2.000, 0.048, 0.063, 0.009, 0.414, 4.566, 0.049, 24.999, 0.001, 0.213, 0.002, 24.326, 0.036, 0.000) #model 4.0 CT18
-if(PDFinUse=="MSHT20"):    
-    initialValues=(2.000, 0.045, 0.184, 0.097, 0.446, 0.966, 0.022, 46.341, 0.002, 2.661, 0.010, 7.659, 65.137, 0.000) #model 4.0 MHST20
-if(PDFinUse=="CJ15"):    
-    initialValues=(2.000, 0.037, 0.147, 0.989, 0.269, 9.893, 0.241, 70.096, 0.024, 3.355, 0.001, 9.262, 130.386, 0.000) #model 4.0 CJ15
+    initialValues=(2.000, 0.048, 0.063, 0.009, 0.414, 4.566, 0.049, 24.999, 0.000, 0.213, 0.002, 24.326, 0.036, 0.000) #model 4.0 CT18
 harpy.setNPparameters(list(initialValues))
 
 DataProcessor.harpyInterface.PrintChi2Table(setDY,printDecomposedChi2=True)
+
+#%%
+#### translation of parameters to list in proper order
+
+p_names=['BNP','c0','l1','l2','l3','l4','l5','l6','l7','l8','l9','l10','l11','l12']
+def params_to_array(params):
+    return [params[p_names[i]].value for i in range(len(params))]
+
 #%%
 #######################################
 # Main chi2 formula
 #######################################
 totalN=setDY.numberOfPoints
 
-
-#### The penalty term accounts for possible penalty effects. 
-#### This one prevents the fNP be too different for different flavors
-def PenaltyTerm(x):
-    r1=0.1*max(x[2]/x[4],x[4]/x[2])
-    r2=0.1*max(x[3]/x[5],x[5]/x[3])
-    r3=0.1*max(x[6]/x[8],x[8]/x[6])
-    r4=0.1*max(x[7]/x[9],x[9]/x[7])
-    
-    res=0
-    if(r1>1): res+=0.1*(r1-1.)
-    if(r2>1): res+=0.1*(r2-1.)
-    if(r3>1): res+=0.1*(r3-1.)
-    if(r4>1): res+=0.1*(r4-1.)
-    
-    return res
-    
-
-def chi_2(x):
+def chi_2(pars):
     startT=time.time()
+    
+    x=params_to_array(pars)
     harpy.setNPparameters(x)
     print('np set =',["{:8.3f}".format(i) for i in x], end =" ")    
     
     ccDY2,cc3=DataProcessor.harpyInterface.ComputeChi2(setDY)
-    
-    if(usePenaltyTerm): ccDY2+=totalN*PenaltyTerm(x)
     
     cc=ccDY2/totalN
     endT=time.time()
     print(':->',cc,'       t=',endT-startT)
     return ccDY2
 
-#%%
-#######################################
-# Setting-up Minuit
-#######################################
-from iminuit import Minuit
-
-##### model 1.0
-#initialErrors=(0.1,       0.002,      0.05,    0.2,  0.05,   0.2,    10.0,   0.1,   0.1)
-#searchLimits=((1.4,4.5), (0.0001,5.0),(0.0,2.0),(0.,25.0),(0.0,2.0),(0.,25.0),(0.,1000),None, None)
-#parametersToMinimize=(True,     False,    False,    False,    False,     False,  False, True, True)
-
-##### model 2.0
-# initialErrors=(0.1,       0.002,      0.05,    0.2,  0.05,    0.2,  0.05,   0.2,    10.0)
-# searchLimits=((1.4,4.5), (0.0001,5.0),(0.0,10.0),(0.,50.0),(0.0,10.0),(0.,50.0),(0.0,10.0),(0.,50.0),(0.,2500))
-# parametersToMinimize=(True,     False,    False,    False,    False,     False,  False, False, False)
-
-# ##### model 4.0
-initialErrors=(0.1,       0.002,      0.05,    0.2,  0.05,   0.2, 0.05,    0.2,0.05,    0.2,   0.05,    0.2, 10., 0.1)
-searchLimits=((1.4,4.5), (0.0001,5.0),(0.001,2.0),(0.001,100.0),(0.001,2.0),(0.001,100.0),(0.001,2.0),
-              (0.001,100.0),(0.001,2.0),(0.001,100.0),(0.001,2.0),(0.001,100.0),(0.001,2500),(0.,100))
-parametersToMinimize=(True,     False,    False,    False,    False,     False,  False, False, False, False, False, False, False,True)
-
 
 #%%
+from lmfit import Parameters, fit_report, minimize
 
-m = Minuit(chi_2, initialValues)
+##### for test I start from CT18 case
+initialValues=(2.000, 0.048, 0.063, 0.009, 0.414, 4.566, 0.049, 24.999, 0.000, 0.213, 0.002, 24.326, 0.036, 0.000)
 
-m.errors=initialErrors
-m.limits=searchLimits
-m.fixed=parametersToMinimize
-m.errordef=1
+fit_params = Parameters()
+fit_params.add_many((p_names[0] , initialValues[0] , False, 1.9, 2.1, None, None),
+                    (p_names[1] , initialValues[1] , True , 0.0001, 1.0, None, None),
+                    (p_names[2] , initialValues[2] , True , 0.001, 3.0, None, None),
+                    (p_names[3] , initialValues[3] , True , 0.001, 25.0, None, None),
+                    (p_names[4] , initialValues[4] , True , 0.001, 3.0, None, None),
+                    (p_names[5] , initialValues[5] , True , 0.001, 25.0, None, None),
+                    (p_names[6] , initialValues[6] , True , 0.001, 3.0, None, None),
+                    (p_names[7] , initialValues[7] , True , 0.001, 25.0, None, None),
+                    (p_names[8] , initialValues[8] , True , 0.001, 3.0, None, None),
+                    (p_names[9] , initialValues[9] , True , 0.001, 25.0, None, None),
+                    (p_names[10], initialValues[10], True , 0.001, 3.0, None, None),
+                    (p_names[11], initialValues[11], True , 0.001, 25.0, None, None),
+                    (p_names[12],  initialValues[12], True , 0.001, 2500., None, None),
+                    (p_names[13], initialValues[13], False, None, None, None, None)
+                    )
 
-print(m.params)
+#%%
+#out = minimize(chi_2, fit_params, method= 'Powell',options={'ftol' : 0.0001})
+#out = minimize(chi_2, fit_params, method= 'Nelder-Mead',options={'fatol' : 0.01})
+out = minimize(chi_2, fit_params, method= 'COBYLA',options={'tol' : 0.0001})
 
-m.tol=0.0001*totalN*10000 ### the last 0.0001 is to compensate MINUIT def
-m.strategy=1
-m.migrad()
-
-## print parameters
-print(m.params)
-
-## print chi^2 table
-harpy.setNPparameters(list(m.values))
-
-print('PENALTY = ',PenaltyTerm(list(m.values)))
 
 DataProcessor.harpyInterface.PrintChi2Table(setDY,printDecomposedChi2=True)
 
-DataProcessor.harpyInterface.PrintChi2Table(setDYfull,printDecomposedChi2=True)
+print(fit_report(out))
+
+#print('------POWELL--')
+#print('------Nelder-Mead--')
+print('------COBYLA--')
 
 sys.exit()
+
 #%%
 #######################################
 # Generate replica of data and compute chi2
@@ -485,7 +391,6 @@ def MinForReplica():
         print('np set =',["{:8.3f}".format(i) for i in x], end =" ")    
         
         ccDY2,cc2=DataProcessor.harpyInterface.ComputeChi2(repDataDY)
-        if(usePenaltyTerm): ccDY2+=totalN*PenaltyTerm(x)
         
         cc=(ccDY2)/totalN
         endT=time.time()
@@ -494,19 +399,16 @@ def MinForReplica():
     
     repDataDY=setDY
     
-    localM = Minuit(repchi_2, initialValues)
+    # localM = Minuit.from_array_func(repchi_2, initialValues,
+    #   error=initialErrors, limit=searchLimits, fix=parametersToMinimize, errordef=1)
     
-    localM.errors=initialErrors
-    localM.limits=searchLimits
-    localM.fixed=parametersToMinimize
-    localM.errordef=1    
-    localM.tol=0.0001*totalN*10000 ### the last 0.0001 is to compensate MINUIT def
-    localM.strategy=1
+    # localM.tol=0.0001*totalN*10000 ### the last 0.0001 is to compensate MINUIT def
+    # localM.strategy=1
 
-    localM.migrad()
+    # localM.migrad()
     
     ### [chi^2, NP-parameters]
-    return [localM.fval,list(localM.values)]
+    # return [localM.fval,localM.values.values()]
 
 #%%
 #######################################
@@ -695,9 +597,9 @@ for i in range(StartReplica,FinalReplica+1):
     print('------------REPLICA ',i,' from [',StartReplica,' , ',FinalReplica,']------------------')
     print('---------------------------------------------------------------')
     
-    ## reset PDF    
-    harpy.setPDFreplica(i)
+    ## reset PDF
     harpy.setNPparameters(initializationArray)    
+    harpy.setPDFreplica(i)
     SaveToLog("Start computation of replica "+str(i) +"in ["+ str(StartReplica)+','+str(FinalReplica)+"]")
     
     ## got to pseudo-data and minimization
@@ -712,12 +614,8 @@ for i in range(StartReplica,FinalReplica+1):
     ## save to file
     f=open(replicaFile,"a+")
     print('SAVING >>  ',f.name)
-    ### [total chi^2(cenral), total chi^2 (pseudo data), list of chi^2 for experiments(central), 
-    ############# number of PDF, list of NP-parameters],penalty term(if used) ]
-    if(usePenaltyTerm): 
-        f.write(str([mainDY,repRes[0],mainDY2,i,repRes[1],PenaltyTerm(repRes[1])])+"\n")
-    else:
-        f.write(str([mainDY,repRes[0],mainDY2,i,repRes[1]])+"\n")
+    ### [total chi^2(cenral), total chi^2 (pseudo data), list of chi^2 for experiments(central), number of PDF, list of NP-parameters]
+    f.write(str([mainDY,repRes[0],mainDY2,i,repRes[1]])+"\n")
     f.close()
     
     print('SAVING >>  PLOTS')
